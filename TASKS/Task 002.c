14e80df40bca1b378507b131fc0f342bfe822421
AmaraChi <99175765+Amah-amara@users.noreply.github.com>
2023-02-14 02:48:59 +0100

    Update Task 002.c

--- TASKS/Task 002.c
+++ TASKS/Task 002.c
@@ -4,3 +4,24 @@ If the score is less than 80, display to the user that they
 are not elgible to be enrolled.
 If the score is greater than or equal 80, they can be enrolled.
 */
+
+#include <stdio.h>
+
+int main()
+{
+  int score;
+  
+  printf("enter your score: ");
+  scanf("%d", &score);
+  
+  if (score < 80)
+    {
+    printf("you are not eligible to enrol");
+  }
+  
+  else if (score >= 80)
+    {
+    printf("you are eligible to enrol");
+  }
+  return 0;
+ }
