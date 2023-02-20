dd10c1a5f36a24d26913eb04486dfc9428159383
AmaraChi <99175765+Amah-amara@users.noreply.github.com>
2023-02-20 22:05:49 +0100

    Update Task 007.c

--- TASKS/Task 007.c
+++ TASKS/Task 007.c
@@ -3,3 +3,16 @@ write your solution after the comment
 Write a function that prints the alphabet, in uppercase,
 followed by a new line.
 */
+
+#include <stdio.h>
+
+void main()
+{
+  char letter;
+  
+  for (letter = 'A'; letter <= 'Z'; letter++)
+  {
+    printf("%c ", letter);
+  }
+  printf("\n");
+}
