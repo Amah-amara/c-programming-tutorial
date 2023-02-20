a655d07e12dff8f644536d96329f7b262b4ede66
AmaraChi <99175765+Amah-amara@users.noreply.github.com>
2023-02-20 21:12:50 +0100

    Update Task 006.c

--- TASKS/Task 006.c
+++ TASKS/Task 006.c
@@ -2,3 +2,22 @@
 Write a program in C to check a given number is even
 or odd using the function.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter a number: ");
+    scanf("%d", &num);
+    
+    if (num % 2 != 0)
+    {
+        printf("This number is odd");
+    }
+    else 
+    {
+        printf("This number is even");
+    }
+    return 0;
+}
