d41d32caa7fc6b72a6cc8ced8082074a1c355100
AmaraChi <99175765+Amah-amara@users.noreply.github.com>
2023-02-20 20:47:12 +0100

    Update Task 005.c

--- TASKS/Task 005.c
+++ TASKS/Task 005.c
@@ -2,3 +2,36 @@
  Write a program in C to find the square of any number
 using the function.
 */
+
+#include <stdio.h>
+#include <math.h>
+
+double sqNum(int);
+
+int main()
+{
+    double num;
+    double result = 0.0;
+    
+   
+    printf("enter a number: ");
+    scanf("%lf", &num);
+    
+    result = sqNum(num);
+    printf("the square of %lf is %0.2lf", num, result);
+    
+    return 0;
+    
+}
+
+double sqNum(int var)
+{
+    if (var == 0)
+    {
+    printf("not a valid number\n");
+    }
+    else
+    {
+    return pow(var, 2);
+    }
+}
