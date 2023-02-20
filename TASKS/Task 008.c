9781d98b929e36a25359fc38973b774724e51146
AmaraChi <99175765+Amah-amara@users.noreply.github.com>
2023-02-20 22:09:47 +0100

    Update Task 008.c

--- TASKS/Task 008.c
+++ TASKS/Task 008.c
@@ -2,3 +2,16 @@
 Write a function that prints the alphabet, in lowercase
 followed by a new line.
 */
+
+#include <stdio>
+
+void main()
+{
+  char letter;
+  
+  for (letter = 'a'; letter <= 'z'; letter++)
+  {
+    printf("%c ", letter)
+  }
+  printf("\n");
+}
